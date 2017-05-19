1b19aeab0f6ca729bf39ee6a125affc9a41156a2
unknown <>
2017-05-20 02:15:47 +0800

    update methods...

--- zepto.js
+++ zepto.js
@@ -1,25 +1,31 @@
 (function(window) {
-
   // 定义一个对象
   var Zepto = function(selector) {
-    this.element = [];
+    this.elements = [];
     return this.$all(selector);
   };
   // Zepto原型定义方法
   Zepto.prototype = {
-
+    // 选择属性
     $all: function(ctx) {
-
       this.elements = window.document.querySelectorAll(ctx);
-      return this.elements;
-
+      return this;
+    },
+    // methods
+    hide : function() {
+      for(let i = 0;i<this.elements.length;i++) {
+        this.elements[i].style.display = 'none';
+      }
+      return this;
+    },
+    show : function() {
+      for(let i = 0;i<this.elements.length;i++) {
+        this.elements[i].style.display = 'block';
+      }
+      return this;
     }
-
   }
-
   window.$ = function(ctx) {
     return new Zepto(ctx);
   }
-
-
-})(window)
+})(window);
